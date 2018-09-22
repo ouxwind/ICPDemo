8806df97a89eee364024d6b410503fd59a28b3b7
GTouxiang <43160890+GTouxiang@users.noreply.github.com>
2018-09-22 08:03:46 +0800

    Initial commit

---
+++ test/com/journaldev/spring/dao/test/StringTest.java
@@ -0,0 +1,15 @@
+package com.journaldev.spring.dao.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringTest {
+
+  @Test
+  public void testEqual() {
+    assertEquals("3", "3");
+
+  }
+
+}
